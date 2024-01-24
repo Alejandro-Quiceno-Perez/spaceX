9b8604ac8a7cdc9b8292f69b5a1dce14243a87fd
Alejandro Quiceno Pérez <89657329+Alejandro-Quiceno-Perez@users.noreply.github.com>
2024-01-24 09:05:43 -0500

    avance

--- app.js
+++ app.js
@@ -24,14 +24,41 @@ function printCards(data) {
             <div class="card-body text-white">
                 <h5 class="card-title">${e.mission_name}</h5>
                 <p class="card-text">${e.launch_year}</p>
-                <a href="#" onclick="console.log(${e.flight_number})" class="btn btn-primary">Ver mas</a>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop" onclick="console.log(${modal()})" class="btn btn-primary">Ver mas</button>
             </div>
         </div>
     `
-
-    // containerCards.appendChild(btnVerMas);
     });
-    
+
 }
 
-getData()
+function modal() {
+    const modal = document.querySelector("#modal");
+    modal.innerHTML = `
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+            aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Understood</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    `
+}
+
+
+
+
+/* Funciones */
+getData();
